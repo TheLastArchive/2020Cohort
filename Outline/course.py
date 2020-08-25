d025671008cc08007f633e3ea67888702ad40a31
jwolf <jwolf@student.wethinkcode.co.za>
2020-08-25 10:18:44 +0200

    Completed Outline

--- Outline/course.py
+++ Outline/course.py
@@ -1,33 +1,43 @@
 
-
-def create_outline():
+def create_outline(): #This assignment has caused me so much physical pain
 
     problems = ["Problem 1", "Problem 2", "Problem 3"]    
-    topics = {"* Introduction to Python": problems,
-            '* Tools of the Trade': problems,
-            '* How to make decisions': problems,
-            '* How to repeat code': problems,
-            '* How to structure data': problems,
-            '* Functions': problems,
-            '* Modules': problems}
+    topics = {"* Introduction to Python",
+            '* Tools of the Trade',
+            '* How to make decisions',
+            '* How to repeat code',
+            '* How to structure data',
+            '* Functions',
+            '* Modules'}
     status = ["STARTED", "GRADED", "COMPLETED"]
+    topics_list = list(topics)
+    topics_list = sorted(topics_list)
+    topics_dict = {topics_list[0] : problems,   #You can pinpoint the exact moment I stopped caring
+                topics_list[1] : problems,
+                topics_list[2] : problems,
+                topics_list[3] : problems,
+                topics_list[4] : problems,
+                topics_list[5] : problems,
+                topics_list[6] : problems}
 
-    fred = (1, "Fred", "Modules", status[1])
-    jeff = (2, "Jeff", "Functions", status[0])
-    victoria = (3, "Victoria", "How to repeat code", status[2])
-    students = [fred, jeff, victoria]
+    students = [("Fred", topics_list[1], problems[2], status[1]),
+                ("Jeff", topics_list[4], problems[0], status[0]),
+                ("Sarah", topics_list[0], problems[1], status[2])]
+    students.sort(key = lambda x:x[3], reverse = True)
 
     print("Course Topics:")
-    for x in topics:
+    for x in topics_list:
         print(x)
     
     print("Problems:")
-    for key, value in topics.items():
-        print(key + ": " + ", ".join(value))
+    for key, value in topics_dict.items():
+        print(key + " : " + ", ".join(value))
 
+    count = 1
     print("Student Progress: ")
     for x in students:
-        print(str(x[0]) + ".", x[1] + " -", x[2], "[" + x[3] + "]")
+        print(str(count) + ". " + x[0] + " -", x[1], x[2], "- ", "[" + x[3] + "]")
+        count += 1
 
 
 if __name__ == "__main__":
